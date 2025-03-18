f2b1071456dbebff2109ac4a12cfa35581fcc68b
Najib <najibtahiri0214@gmail.com>
2025-03-17 20:47:15 -0400

    Delete kingdom_of_cards directory

--- kingdom_of_cards/deck.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE deck (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    user_id INT NOT NULL,
-    card_id INT NOT NULL,
-    position INT NOT NULL, -- Position de 1 à 10 (ordre des cartes)
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (card_id) REFERENCES cards(id)
-);
--- kingdom_of_cards/inventory.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE inventory (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    user_id INT NOT NULL,
-    card_id INT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (card_id) REFERENCES cards(id)
-);
